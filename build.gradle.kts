ffe9ebba203fa548411271cf0a06e631ad23cd0f
AndreSuryana <Andresuryana17@gmail.com>
2025-05-05 15:41:03 +0700

    ci: configure Maven Central publishing and GitHub Actions
    - Added Gradle publishing configuration for Maven Central
    - Created `publish.yml` to automate release deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    id("com.vanniktech.maven.publish") version "0.31.0"
 }
+
+group = "io.github.skyroute"
+version = "0.1.0-alpha.1"
 
 subprojects {
     apply(plugin = "skyroute-spotless")
--- sky-service/build.gradle.kts
+++ sky-service/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("com.vanniktech.maven.publish")
 }
 
 android {
@@ -45,4 +48,37 @@ dependencies {
 
     // Paho MQTT Client
     implementation(libs.paho.client)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates(rootProject.group.toString(), name, rootProject.version.toString())
+
+    pom {
+        name = "SkyRouteMQ"
+        description = "A lightweight and reliable MQTT connection manager for Android, " +
+            "designed to handle long-running and persistent connections with minimal overhead"
+        inceptionYear = "2025"
+        url = "https://github.com/skyroute/skyroute-mq"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "andresuryana"
+                name = "Andre Suryana"
+                url = "https://github.com/andresuryana"
+            }
+        }
+        scm {
+            url = "https://github.com/skyroute/skyroute-mq/"
+            connection = "scm:git:git://github.com/skyroute/skyroute-mq.git"
+            developerConnection = "scm:git:ssh://git@github.com/skyroute/skyroute-mq.git"
+        }
+    }
 }
